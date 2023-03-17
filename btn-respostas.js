b615dae473139555f56f05fc6155230ca08dfde1
DevGomes2022 <kelriytff@gmail.com>
2023-03-17 17:20:12 -0300

    Sexto commit
    Criei algumas abas de questões, e o botão de aparecer e sumir as respostas, falta resolver o problema de centralizar as questões no meio da tela e criar o botão de passar pra próxima aba (1,2,3,4,5...)

---
+++ btn-respostas.js
@@ -0,0 +1,59 @@
+const btnA = document.getElementById("btn-resolucao-a");
+    const infoA = document.getElementById("infoA");
+    btnA.addEventListener("click", () => {
+    if (infoA.style.display === "block") {
+        infoA.style.display = "none";
+    } else {
+        infoA.style.display = "block";
+    }
+});
+
+const btnB = document.getElementById("btn-resolucao-b");
+    const infoB = document.getElementById("infoB");
+    btnB.addEventListener("click", () => {
+    if (infoB.style.display === "block") {
+        infoB.style.display = "none";
+    } else {
+        infoB.style.display = "block";
+    }
+});
+
+const btnC = document.getElementById("btn-resolucao-c");
+    const infoC = document.getElementById("infoC");
+    btnC.addEventListener("click", () => {
+    if (infoC.style.display === "block") {
+        infoC.style.display = "none";
+    } else {
+        infoC.style.display = "block";
+    }
+});
+
+const btnD = document.getElementById("btn-resolucao-d");
+    const infoD = document.getElementById("infoD");
+    btnD.addEventListener("click", () => {
+    if (infoD.style.display === "block") {
+        infoD.style.display = "none";
+    } else {
+        infoD.style.display = "block";
+    }
+});
+
+const btnE = document.getElementById("btn-resolucao-e");
+    const infoE = document.getElementById("infoE");
+    btnE.addEventListener("click", () => {
+    if (infoE.style.display === "block") {
+        infoE.style.display = "none";
+    } else {
+        infoE.style.display = "block";
+    }
+});
+
+const btnF = document.getElementById("btn-resolucao-f");
+    const infoF = document.getElementById("infoF");
+    btnF.addEventListener("click", () => {
+    if (infoF.style.display === "block") {
+        infoF.style.display = "none";
+    } else {
+        infoF.style.display = "block";
+    }
+});
